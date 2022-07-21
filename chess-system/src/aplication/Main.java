3d2a97836d0759d63b38caccf67f534b5e288ab9
Dhionatan <dhionatan.dvg@gmail.com>
2022-07-20 21:09:26 -0300

    Criado a classe Position

--- chess-system/src/aplication/Main.java
+++ chess-system/src/aplication/Main.java
@@ -1,7 +1,11 @@
 package aplication;
 
+import bordgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(3,5);
+
+        System.out.println(position);
     }
 }
