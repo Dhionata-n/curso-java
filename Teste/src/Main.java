058f3e6fe7f89b2200cbf73173052072ed3b8f19
Dhionatan <dhionatan.dvg@gmail.com>
2022-07-21 21:22:36 -0300

    ChessLayer

---
+++ Teste/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = 1;
+        int soma = 0;
+        while (numero < 5) {
+            soma += numero;
+            numero ++;
+
+        }
+        System.out.println(soma);
+
+        }
+
+    }
